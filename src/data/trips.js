418990f01d073041191c716485c98748f1ed8b01
TobiasBak <tobiasbak@live.dk>
2025-12-16 10:14:39 +0100

    New method of loading images from folders

--- src/data/trips.js
+++ src/data/trips.js
@@ -1,53 +1,42 @@
-import day1 from '../assets/images/day/andreas-gucklhorn-mawU2PoJWfU-unsplash.jpg';
-import day2 from '../assets/images/day/ethan-robertson-SYx3UCHZJlo-unsplash.jpg';
-import day3 from '../assets/images/day/luca-bravo-ii5JY_46xH0-unsplash.jpg';
-import night1 from '../assets/images/night/jonatan-pie-3l3RwQdHRHg-unsplash.jpg';
-import night2 from '../assets/images/night/jonatan-pie-h8nxGssjQXs-unsplash.jpg';
-
 export const trips = [
   {
     id: 1,
+    filename: 'jonatan-pie-3l3RwQdHRHg-unsplash.jpg',
     title: 'Neon Dreams',
     location: 'Hong Kong',
-    image: night1,
     date: 'November 2023',
-    isNight: true,
     size: 'tall'
   },
   {
     id: 2,
+    filename: 'andreas-gucklhorn-mawU2PoJWfU-unsplash.jpg',
     title: 'Summer in Italy',
     location: 'Amalfi Coast',
-    image: day1,
     date: 'June 2024',
-    isNight: false,
     size: 'normal'
   },
   {
     id: 3,
+    filename: 'jonatan-pie-h8nxGssjQXs-unsplash.jpg',
     title: 'Midnight in Paris',
     location: 'France',
-    image: night2,
     date: 'September 2023',
-    isNight: true,
     size: 'wide'
   },
   {
     id: 4,
+    filename: 'ethan-robertson-SYx3UCHZJlo-unsplash.jpg',
     title: 'Desert Vibes',
     location: 'Joshua Tree',
-    image: day2,
     date: 'March 2024',
-    isNight: false,
     size: 'normal'
   },
   {
     id: 5,
+    filename: 'luca-bravo-ii5JY_46xH0-unsplash.jpg',
     title: 'Nordic Escape',
     location: 'Iceland',
-    image: day3,
     date: 'December 2023',
-    isNight: false,
     size: 'big'
   }
 ];
